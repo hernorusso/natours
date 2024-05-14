338ef953e7372731b2f2a72fa1e4c38baced9dc0
herno <herno.russo@gmail.com>
2024-05-14 16:59:45 -0300

    Add Aggregation pipeline
    Use for present data stats

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,11 +10,15 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = tourController;
 
 // Routes
-router.route('/').get(getAllTours).post(createTour);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
+router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
